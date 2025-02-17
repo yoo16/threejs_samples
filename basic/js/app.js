0e6f60cd8661eebecbe41e275a442e8b1299f3ba
yoo16 <yohei.yoshikawa@gmail.com>
2025-02-17 20:02:07 +0900

    update

--- basic/js/app.js
+++ basic/js/app.js
@@ -154,10 +154,3 @@ function animate() {
     renderer.render(scene, camera);
 }
 animate();
-
-// ウィンドウリサイズ対応
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
